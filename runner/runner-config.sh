ee228a97aa73c3cf6328478c26533722eadf4774
Joby Pooppillikudiyil <joby@Jobys-MBP.fritz.box>
2024-06-20 19:00:32 +0200

    moving config

--- runner/runner-config.sh
+++
@@ -1,7 +0,0 @@
-# runner-config.sh
-
-export CONFIG_URL=${CONFIG_URL}
-export CONFIG_TOKEN=${CONFIG_TOKEN}
-export CONFIG_RUNNERGROUP=nextgen-runners
-export CONFIG_LABELS=${CONFIG_LABELS}
-export CONFIG_NAME=$(hostname)
