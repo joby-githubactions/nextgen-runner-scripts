22a231ce4d234e4c1ce120eda463128e10cbf4cb
Joby Pooppillikudiyil <joby@Jobys-MacBook-Pro.fritz.box>
2024-07-05 18:56:30 +0200

    helm fix

--- helm/helm_build_push.sh
+++ helm/helm_build_push.sh
@@ -182,9 +182,9 @@ helm template ${helm_template_folder}
 print_color "32;1" "Doing helm package"
 helm package ${helm_template_folder}
 
-local helm_chart_name="${APPLICATION_NAME}-${BUILD_VERSION}.tgz"
-local helm_chart_location="$(pwd)/${helm_chart_name}"
-local helm_chart="${output_folder}/${helm_chart_name}"
+helm_chart_name="${APPLICATION_NAME}-${BUILD_VERSION}.tgz"
+helm_chart_location="$(pwd)/${helm_chart_name}"
+helm_chart="${output_folder}/${helm_chart_name}"
 
 mv "${helm_chart_location}" "${output_folder}/"
 print_color "32;1" "Helm Chart Location: ${helm_chart}"
