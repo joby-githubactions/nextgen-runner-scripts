8db5db797cc8ebab52634550b457eefb7d19ab83
Joby Pooppillikudiyil <joby@Jobys-MacBook-Pro.fritz.box>
2024-07-06 16:05:48 +0200

    token

--- shared/upload_artifact.sh
+++ shared/upload_artifact.sh
@@ -7,9 +7,12 @@ function upload_artifact() {
     print_color "32;1" "Uploading artifact: $filename"
     local filepath="$1"
     local filename=$(basename "$filepath")
-    local api_url="https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/artifacts"
+    local api_url="https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/artifacts
     local token="${GITHUB_TOKEN}"
-    print_color "32;1" "$token"
+    print_color "32;1" "File Path: $filepath"
+    print_color "32;1" "File Name: $filename"
+    print_color "32;1" "API URL:   $api_url"
+    print_color "32;1" "TOKEN:     $token"
 
     print_color "32;1" "Uploading artifact: $filename"
 
