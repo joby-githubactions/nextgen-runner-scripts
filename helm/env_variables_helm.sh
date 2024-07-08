6f1f015cb792a0b89c0f0b5141ae5a87d49cf6e7
Joby Pooppillikudiyil <joby@Jobys-MacBook-Pro.fritz.box>
2024-07-08 09:41:11 +0200

    file

--- helm/env_variables_helm.sh
+++ helm/env_variables_helm.sh
@@ -59,3 +59,9 @@ export SOURCE_BRANCH="${SOURCE_BRANCH:-$(get_git_branch_name)}"
 # Helm details
 export HELM_EXPERIMENTAL_OCI=1
 export HELM_OCI_URL="${HELM_OCI_URL:-oci://${DOCKER_IMAGE_PUSH_PREFIX}}"
+
+#Optional Ingress details
+export INGRESS_HOST="${INGRESS_HOST}"
+
+#Optional Image pull secret
+export IMAGE_PULL_SECRET="${IMAGE_PULL_SECRET}"
